ae88dd43e895e87ea6d2cb185c2477932610a068
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2025-09-12 22:00:17 -0400

    changed on-the-fly filters arguments

--- tests/_test_database.py
+++ tests/_test_database.py
@@ -1,5 +1,12 @@
 import logging
-from sbvoicedb import database, Pathology, Speaker, RecordingSession, Recording
+from sbvoicedb import (
+    database,
+    Pathology,
+    Speaker,
+    RecordingSession,
+    Recording,
+    sql_expr,
+)
 import nspfile
 from pprint import pprint
 
@@ -30,11 +37,14 @@ print(single_case)
 db_laryngitis = db = database.SbVoiceDb(
     "/home/kesh/data/SVD",
     download_mode="lazy",
-    pathology_filter=Pathology.name == "Laryngitis",
-    include_healthy=True,
-    speaker_filter=Speaker.gender == "w",
-    session_filter=RecordingSession.speaker_age.between(50, 70),
+    pathology_filter=Pathology.name.in_(
+        ["Carcinoma in situ", "Dysplastischer Kehlkopf"]
+    ),
+    include_healthy=False,
+    speaker_filter=Speaker.gender == "m",
+    session_filter=RecordingSession.speaker_age.between(50, 75),
     recording_filter=Recording.utterance.in_(("a_n", "phrase")),
+    # echo=True
 )
 
 # db.set_session_filter(~RecordingSession.pathologies.any())
@@ -44,26 +54,17 @@ print(f"number of pathologies found: {db_laryngitis.get_pathology_count()}")
 print(f"number of recording sessions found: {db_laryngitis.get_session_count()}")
 print(f"number of unique speakers: {db_laryngitis.get_speaker_count()}")
 print(f"number of recordings: {db_laryngitis.get_recording_count()}")
+print(db_laryngitis.get_pathology_ids())
+print(db_laryngitis.get_pathology_names())
+for speaker in db_laryngitis.iter_speakers():
+    print(
+        speaker.id,
+        speaker.birthdate,
+        speaker.gender,
+        [s.id for s in speaker.sessions],
+    )
 
-import numpy as np
-from matplotlib import pyplot as plt
-
-rec = next(db_laryngitis.iter_recordings())
-
-t = np.arange(rec.length)/rec.rate
-
-fig, axes = plt.subplots(2, 1, sharex=True)
-axes[0].plot(t,rec.nspdata)
-axes[0].set_ylabel('acoustic data')
-axes[1].plot(t,rec.eggdata)
-axes[1].set_ylabel('EGG data')
-axes[1].set_xlabel('time (s)')
-plt.tight_layout()
-plt.show()
-
-exit()
-
-for session in db.iter_sessions():
+for session in db_laryngitis.iter_sessions():
     print(
         session.id,
         session.speaker.id,
@@ -72,9 +73,11 @@ for session in db.iter_sessions():
         [patho.name for patho in session.pathologies],
     )
 
-for rec in db.iter_recordings(full_file_paths=True):
+for rec in db_laryngitis.iter_recordings(full_file_paths=True):
     print(
         rec.id,
+        rec.length,
+        rec.utterance,
         rec.nspfile,
         rec.eggfile,
         rec.session.id,
@@ -86,13 +89,49 @@ for rec in db.iter_recordings(full_file_paths=True):
 
     assert len(rec.nspdata) == rec.length and len(rec.eggdata) == rec.length
 
-for speaker in db.iter_speakers():
+sbvoicedb = db_laryngitis
+
+assert sbvoicedb.get_recording_count() == 4  # dataset downloading occurs here
+ids = sbvoicedb.get_recording_ids()
+for rec in sbvoicedb.iter_recordings():
+    assert rec.id in ids
     print(
-        speaker.id,
-        speaker.birthdate,
-        speaker.gender,
-        [s.id for s in speaker.sessions],
+        rec.id,
+        rec.nspfile,
+        rec.eggfile,
+        rec.session.id,
+        rec.session.speaker.id,
+        rec.session.speaker_age,
+        rec.session.speaker.gender,
+        [patho.name for patho in rec.session.pathologies],
     )
+    assert len(rec.nspdata) == rec.length and len(rec.eggdata) == rec.length
+
+assert (
+    sbvoicedb.get_recording_count(session_id=820) == 2
+)  # dataset downloading occurs here
+assert (
+    sbvoicedb.get_recording_count(session_filter=RecordingSession.speaker_age > 70) == 2
+)
+
+exit()
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+rec = next(db_laryngitis.iter_recordings())
+
+t = np.arange(rec.length) / rec.rate
+
+fig, axes = plt.subplots(2, 1, sharex=True)
+axes[0].plot(t, rec.nspdata)
+axes[0].set_ylabel("acoustic data")
+axes[1].plot(t, rec.eggdata)
+axes[1].set_ylabel("EGG data")
+axes[1].set_xlabel("time (s)")
+plt.tight_layout()
+plt.show()
+
 
 # pprint(db.get_session_ids_of_all_pathologies(use_name=True))
 # for speaker in speakers:
--- tests/test_all.py
+++ tests/test_all.py
@@ -1,7 +1,12 @@
-from sbvoicedb import SbVoiceDb, Pathology, Speaker, RecordingSession, Recording
+from sbvoicedb import (
+    SbVoiceDb,
+    Pathology,
+    Speaker,
+    RecordingSession,
+    Recording,
+    sql_expr,
+)
 import pytest
-from os import remove
-from shutil import rmtree
 
 
 def load_db():
@@ -12,9 +17,9 @@ def load_db():
             ["Carcinoma in situ", "Dysplastischer Kehlkopf"]
         ),
         include_healthy=False,
-        # speaker_filter=Speaker.gender == "m",
-        # session_filter=RecordingSession.speaker_age.between(60, 75),
-        # recording_filter=Recording.utterance.in_(("a_n", "phrase")),
+        speaker_filter=Speaker.gender == "m",
+        session_filter=RecordingSession.speaker_age.between(50, 75),
+        recording_filter=Recording.utterance.in_(("a_n", "phrase")),
     )
 
 
@@ -25,7 +30,7 @@ def sbvoicedb():
 
 def test_pathology(sbvoicedb):
 
-    assert sbvoicedb.get_pathology_count() > 0
+    assert sbvoicedb.get_pathology_count() ==2
     ids = sbvoicedb.get_pathology_ids()
     names = sbvoicedb.get_pathology_names()
     for patho in sbvoicedb.iter_pathologies():
@@ -34,21 +39,45 @@ def test_pathology(sbvoicedb):
 
 
 def test_speaker(sbvoicedb):
-    assert sbvoicedb.get_speaker_count() > 0
+    assert sbvoicedb.get_speaker_count() ==2
     ids = sbvoicedb.get_speaker_ids()
     for speaker in sbvoicedb.iter_speakers():
         assert speaker.id in ids
 
 
 def test_session(sbvoicedb):
-    assert sbvoicedb.get_session_count() > 0
+    assert sbvoicedb.get_session_count() ==2
     ids = sbvoicedb.get_session_ids()
     for session in sbvoicedb.iter_sessions():
         assert session.id in ids
 
+    assert sbvoicedb.get_session(820) is not None
+    assert sbvoicedb.get_session_count(speaker_id=1432) == 1
+    assert sbvoicedb.get_session_count(pathologies="Carcinoma in situ") == 1
+    assert (
+        sbvoicedb.get_session_count(
+            pathologies=["Carcinoma in situ", "Dysplastischer Kehlkopf"]
+        )
+        == 2
+    )
+    assert sbvoicedb.get_session_count(pathologies=36) == 1
+    assert sbvoicedb.get_session_count(pathologies=[64, 36]) == 2
+    assert (
+        sbvoicedb.get_session_count(session_filter=RecordingSession.speaker_age > 70)
+        == 1
+    )
+    assert (
+        sbvoicedb.get_session_count(
+            recording_filter=sql_expr.and_(
+                Recording.length > 100000, Recording.utterance == "phrase"
+            )
+        )
+        == 1
+    )
+
 
 def test_recording(sbvoicedb):
-    assert sbvoicedb.get_recording_count() > 0  # dataset downloading occurs here
+    assert sbvoicedb.get_recording_count() == 4  # dataset downloading occurs here
     ids = sbvoicedb.get_recording_ids()
     for rec in sbvoicedb.iter_recordings():
         assert rec.id in ids
@@ -63,3 +92,8 @@ def test_recording(sbvoicedb):
             [patho.name for patho in rec.session.pathologies],
         )
         assert len(rec.nspdata) == rec.length and len(rec.eggdata) == rec.length
+    assert sbvoicedb.get_recording_count(session_id=820) == 2
+    assert (
+        sbvoicedb.get_recording_count(session_filter=RecordingSession.speaker_age > 70)
+        == 2
+    )
